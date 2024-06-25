c4aeeea51b003dfe50053c9e2ab49f26385b740f
Tyler Bules <5021085+Troublebrewing@users.noreply.github.com>
2024-06-25 08:10:15 -0400

    fetches from google calendar
    also can convert badge.svg to image and put it on the display. This conversion uses svglib reportlab and ultimately cairo which depends on some external dependancies. The cairo folder has all the dlls from Gimp installation. at least a few are needed but im not sure which ones.

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
+
 import datetime
 from pytz import timezone
 import pytz
@@ -12,6 +13,7 @@ from gcsa.recurrence import Recurrence, DAILY, SU, SA
 
 import epd7in3f
 import time
+import schedule
 
 REFRESH_PERIOD_MINUTES = 5
 
@@ -32,7 +34,7 @@ def make_image(focus_event, in_event):
     im = Image.new(mode="RGB", size=(display_width,display_height), color="white")
 
     im2 = ImageDraw.Draw(im)
-    im2.fontmode = "1"  #turns off anti aliasing. quantization gets weird if this is on
+    im2.fontmode = "1"  #turns off text anti aliasing. quantization gets weird if this is on
 
     #colors
                  #0xBBGGRR
@@ -68,14 +70,25 @@ def make_image(focus_event, in_event):
 
     # battery
 
-    dixon_logo = Image.open("Dixon Logo.png")
-    aspect_ratio = dixon_logo.width / dixon_logo.height
+    badge = Image.open("badge.png")
+    aspect_ratio = badge.width / badge.height
     if status != "Away":  
-        #dixon logo  
-        logo_height = 220
-        dixon_logo = dixon_logo.resize((logo_height,int(logo_height/aspect_ratio)))
+        #badge
+        if badge.height > badge.width:
+            #portrait image. scale via height
+            max_logo_height = 190
+            badge = badge.resize((int(max_logo_height * aspect_ratio),max_logo_height))
+        else:
+            #landscape image. scale via width
+            max_logo_width = 220
+            badge = badge.resize((max_logo_width,int(max_logo_width/aspect_ratio)))
+
+        #dixon_logo = dixon_logo.resize((logo_height,int(logo_height/aspect_ratio)))
         #im2.bitmap((borderwidth+padding+380, borderwidth+padding), dixon_logo, fill="blue")
-        im.paste(dixon_logo,(borderwidth+padding+510, borderwidth+padding),dixon_logo)
+        badge_x_anchor = int(((((display_width)+530)/2))-(badge.width/2))
+        bar_y_anchor = 240
+        badge_y_anchor = int((bar_y_anchor/2)-(badge.height/2))
+        im.paste(badge,(badge_x_anchor, badge_y_anchor))
 
         # name
         name_y_anchor = 70
@@ -88,7 +101,7 @@ def make_image(focus_event, in_event):
         im2.text((borderwidth+padding, title_y_anchor), "ELECTRICAL ENGINEERING MANAGER", font=titlefont, fill=EPD_BLUE)
 
         # bar
-        bar_y_anchor = 240
+        #bar_y_anchor = 240
         im2.line([(borderwidth+padding, bar_y_anchor),(display_width-borderwidth-padding,bar_y_anchor)],fill=EPD_BLACK, width=20)
 
         # static status text
@@ -105,28 +118,26 @@ def make_image(focus_event, in_event):
         im2.text((anchor , status_y_anchor), status.upper(), font=statusfont2, fill=statuscolor)
 
         # available
-        if(in_event):
-            time_free_avail = focus_event.end.strftime("%I:%M %p")
-        else:
-            time_free_avail = focus_event.start.strftime("%I:%M %p")
+        if(focus_event is not None):
+            if(in_event):
+                time_free_avail = focus_event.end.strftime("%I:%M %p")
+            else:
+                time_free_avail = focus_event.start.strftime("%I:%M %p")
         
-        availablility_y_anchor = 420
-        statusfont3 = ImageFont.truetype('Inter/static/Inter-Black.ttf', 35)
-        available_string = ""
-        if(status == "Available"):
-            available_string = "AVAILABLE UNTIL:"
-        else:
-            available_string = "FREE AT:"
+            availablility_y_anchor = 420
+            statusfont3 = ImageFont.truetype('Inter/static/Inter-Black.ttf', 35)
+            available_string = ""
+            if(status == "Available"):
+                available_string = "AVAILABLE UNTIL:"
+            else:
+                available_string = "FREE AT:"
 
-        im2.text((borderwidth+padding, availablility_y_anchor), available_string, font=statusfont, fill=EPD_BLACK)
-        boundingbox = im2.textbbox((borderwidth+padding, availablility_y_anchor), available_string, font=statusfont)
+            im2.text((borderwidth+padding, availablility_y_anchor), available_string, font=statusfont, fill=EPD_BLACK)
+            boundingbox = im2.textbbox((borderwidth+padding, availablility_y_anchor), available_string, font=statusfont)
 
         
-        im2.text((boundingbox[2]+20, availablility_y_anchor), time_free_avail, font=statusfont3, fill=EPD_BLACK)
-        boundingbox = im2.textbbox((boundingbox[2]+20, availablility_y_anchor), str(time_free_avail), font=statusfont3)
-
-        #anchor = boundingbox[2]+padding
-        #im2.text((anchor, availablility_y_anchor), "MINUTES", font=statusfont3, fill=(0, 0, 0))
+            im2.text((boundingbox[2]+20, availablility_y_anchor), time_free_avail, font=statusfont3, fill=EPD_BLACK)
+            boundingbox = im2.textbbox((boundingbox[2]+20, availablility_y_anchor), str(time_free_avail), font=statusfont3)
 
     else:
         logo_height = 600
@@ -137,10 +148,12 @@ def make_image(focus_event, in_event):
     return im
 
 def GetFocusEvent(todays_events):
-    dummy_time_start = datetime.datetime.now() + datetime.timedelta(days=1)
-    dummy_time_end  = datetime.datetime.now() + datetime.timedelta(days=2)
-    focus_event = Event("Focus Event", start=dummy_time_start,end=dummy_time_end)
-    
+    #dummy_time_start = datetime.datetime.now() + datetime.timedelta(days=1)
+    #dummy_time_end  = datetime.datetime.now() + datetime.timedelta(days=2)
+    #focus_event = Event("Focus Event", start=dummy_time_start,end=dummy_time_end)
+
+    focus_event = None
+
     for event in todays_events:
         #start_string = ""
         #if(isinstance(event.start, datetime.date)):
@@ -174,32 +187,26 @@ def In_event(event):
     utcnow = datetime.datetime.now(datetime.UTC)
     now_local = utcnow.astimezone(timezone('US/Eastern'))
     
-    if((now_local > event.start) & (now_local < event.end)):
-        in_event = True                
-    
+    try:
+        if((now_local > event.start) & (now_local < event.end)):
+            in_event = True 
+    except:               
+        return in_event
     return in_event
 
-
-epd = epd7in3f.EPD()
-
-pcp = serial.Serial('COM5', 115200, 8, "N", 1, timeout=10, rtscts=1)
-
-test = 0
-if(test == 0):
-    mystring = """AT+RUN "$autorun"\r\n"""
-    pcp.write(mystring.encode())
-    time.sleep(1)
-
-calendar = GoogleCalendar('tbules@dixonvalve.com',authentication_flow_port=8081)
-utc = pytz.UTC
-
-while(True):
-    tomorrow_date = datetime.date.today() + datetime.timedelta(days=1)
-    todays_events = calendar.get_events(time_min=datetime.datetime.now(), time_max=tomorrow_date)
-
+def UpdateDisplay():
+    UpdateEvents()
+    
+    print("Updating Display...")
+    
+    #global todays_events
+    global todays_events
     focus_event = GetFocusEvent(todays_events)
-
-    print("Focus Event:"+focus_event.summary)
+    
+    try:
+        print("Focus Event:"+focus_event.summary)
+    except:
+        print("No focus event")
 
     in_event = In_event(focus_event)
 
@@ -212,4 +219,54 @@ while(True):
         pcp.write(wavesharebuf)
         print("done\n")
 
-    time.sleep(60*REFRESH_PERIOD_MINUTES)
+
+def UpdateEvents():
+    print("Fetching latest events...")
+    tomorrow_date = datetime.date.today() + datetime.timedelta(days=1)
+    global todays_events
+    global calendar
+    todays_events = calendar.get_events(time_min=datetime.datetime.now(), time_max=tomorrow_date)
+
+from svglib.svglib import svg2rlg
+
+import os
+folder_path = os.path.abspath("./cairo/bin/")
+path_env_var = os.environ["PATH"]
+if folder_path not in path_env_var:
+    os.environ["PATH"] = folder_path + os.pathsep + path_env_var
+
+from reportlab.graphics import renderPM
+import io
+
+vector_badge = svg2rlg("badge.svg")
+place_holder = io.BytesIO()
+#renderPM.drawToFile(vector_badge, place_holder, fmt="PNG")
+renderPM.drawToFile(vector_badge, "badge.png", fmt="PNG", dpi=100,)
+#place_holder.show()
+
+#import cairosvg 
+#cairosvg.svg2png(file_obj=open("/badge.svg",'r'),output_width=200, write_to="badge.png")
+
+epd = epd7in3f.EPD()
+
+pcp = serial.Serial('COM5', 115200, 8, "N", 1, timeout=10, rtscts=1)
+
+test = 0
+if(test == 0):
+    mystring = """AT+RUN "$autorun"\r\n"""
+    pcp.write(mystring.encode())
+    time.sleep(1)
+
+calendar = GoogleCalendar('tbules@dixonvalve.com', save_token=True, authentication_flow_port=8081)
+
+utc = pytz.UTC
+
+#UpdateEvents()
+UpdateDisplay()
+
+#schedule.every(15).minutes.do(UpdateEvents)
+schedule.every(5).minutes.do(UpdateDisplay)
+
+while(True):
+    schedule.run_pending()
+    time.sleep(1)
