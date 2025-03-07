aa0a43ead7b0cd31dfb9a5632c25549f95f4b145
Tyler Bules <tbules@dixonvalve.com>
2025-03-07 15:01:09 -0500

    major overhaul from tk to customtkinter

--- server2.py
+++ server2.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+import customtkinter
 from PIL import Image, ImageTk
 
 import json
@@ -7,109 +8,83 @@ import json
 import asyncio
 from bleak import BleakClient, BleakScanner
 
-theme = "dark"
+class PresetListFrame(customtkinter.CTkScrollableFrame):
+    def __init__(self, master):
+        super().__init__(master)        
+        self.title = "Presets"    
 
-if theme == "dark":
-    bg_color = '#333'
-    fg_color = '#fff'
-if theme == "light":
-    bg_color = '#fff'
-    fg_color = '#000'
+        #self.configure(border_width=0, ipadx=0, ipady=0, padx=0, pady=0)
 
-class App:
-    def __init__(self, root):
-        self.root = root
-        self.root.geometry("1020x560")
-        self.root.resizable(False, False)
-        self.root.title("Image Viewer")
-
-        # Left frame for presets
-        self.left_frame = tk.Frame(root, bg=bg_color) #frame will grow or shrink to size of widgets within frame
-        self.left_frame.pack(side=tk.LEFT, fill=tk.Y)
-
-        #the word preset
-        self.preset_label = tk.Label(self.left_frame, text="Presets:", bg=bg_color, fg=fg_color, font=("Helvetica", 10, "bold"))
-        self.preset_label.pack(fill=tk.X, expand=True)
-
-        # Scrollbar for presets
-        self.left_canvas = tk.Canvas(self.left_frame, width=200, height=530, bg=bg_color)
-        self.scrollbar = ttk.Scrollbar(self.left_frame, orient=tk.VERTICAL, command=self.left_canvas.yview)
-        self.scrollbar.configure(style="Vertical.TScrollbar")
-        style = ttk.Style()
-        style.configure("Vertical.TScrollbar", background=bg_color)
-        self.scrollable_frame = tk.Frame(self.left_canvas, width=200, height=530, bg=bg_color)
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
         
-        self.scrollable_frame.bind(
-            "<Configure>",
-            lambda e: self.left_canvas.configure(
-                scrollregion=self.left_canvas.bbox("all")
-            )
-        )
+        #self.root = root
+        self.geometry("1020x560")        
+        self.title("Epaper Display Manager")
+        self.grid_columnconfigure(0, weight=1, uniform="group1")
+        self.grid_columnconfigure(1, weight=4, uniform="group1")
+        self.grid_rowconfigure(1, weight=1)
+        self.maxsize(1020,560)
 
-        self.left_canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
-        self.left_canvas.configure(yscrollcommand=self.scrollbar.set)
-
-        #self.left_canvas.pack(side=tk.LEFT, fill=tk.Y, expand=True)
-        self.left_canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-
-        # Bind the mouse wheel to the canvas
-        self.left_canvas.bind_all("<MouseWheel>", self._on_mouse_wheel)
+        # add widgets onto the frame...
+        self.label = customtkinter.CTkLabel(self, text="Presets:")
+        self.label.grid(row=0, column=0, padx=0, pady=0, sticky="nsew")
+        
+        # Left frame for presets
+        self.preset_list_frame = PresetListFrame(self) #frame will grow or shrink to size of widgets within frame
+        self.preset_list_frame.grid(row=1, column=0, ipadx=0, padx=0, pady=0, sticky="nsew")
         
         # Right frame for image display and buttons
-        self.right_frame = tk.Frame(root, width=800, height=480, bg=bg_color)
-        #self.right_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
-        self.right_frame.pack(side=tk.RIGHT)
+        self.right_frame = customtkinter.CTkFrame(self)
+        self.right_frame.grid(row=0, column=1, padx=0, pady=0, rowspan=2, sticky="nsew")
+
+        self.right_frame.grid_columnconfigure(0, weight=1)
+        self.right_frame.grid_rowconfigure(0, weight=1)
         
-        self.tabControl = ttk.Notebook(self.right_frame) 
-        self.customize_tab = ttk.Frame(self.tabControl, style="Custom.TFrame")
-        self.preview_tab = ttk.Frame(self.tabControl, style="Custom.TFrame")
-        style = ttk.Style()
-        style.configure("Custom.TFrame", background=bg_color)
-         
-        
-        self.tabControl.add(self.customize_tab, text ='Customize') 
-        self.tabControl.add(self.preview_tab, text ='Preview') 
-        self.tabControl.pack(expand = 1) 
+        self.tabControl = customtkinter.CTkTabview(self.right_frame) 
+        self.tabControl.add("Customize")
+        self.tabControl.add("Preview")
+        self.tabControl.set("Preview")
+        self.tabControl.grid(row=0, column=0, padx=0, pady=0, sticky="nsew") 
 
         # Canvas for displaying the selected image
-        self.canvas = tk.Canvas(self.preview_tab, width=800, height=480, bg=bg_color)
+        self.canvas = tk.Canvas(self.tabControl.tab("Preview"), width=800, height=480)
         self.canvas.pack()
 
         # Buttons below the image
-        self.control_frame = tk.Frame(self.right_frame, bg=bg_color)
-        self.control_frame.pack(fill=tk.X)
+        self.control_frame = customtkinter.CTkFrame(self.right_frame, fg_color="transparent")
+        self.control_frame.grid(row=1, column=0, padx=0, pady=0, sticky="ew")
 
-        self.refresh_button = tk.Button(self.control_frame, text="Refresh")
+        self.refresh_button = customtkinter.CTkButton(self.control_frame, text="Refresh", command=self.refresh)
         self.refresh_button.pack(side=tk.LEFT, padx=10, pady=10)
-        self.refresh_button.config(command=self.refresh)
 
-        self.upload_button = tk.Button(self.control_frame, text="Upload")
+        self.upload_button = customtkinter.CTkButton(self.control_frame, text="Upload", command=self.upload)
         self.upload_button.pack(side=tk.RIGHT, padx=10, pady=10)
 
-        self.dropdown_var = tk.StringVar(self.control_frame) 
-        #self.dropdown_var.set("Option 1")  # default value
+        self.dropdown_var = customtkinter.StringVar(self.control_frame) 
+        self.dropdown_var.set("Option 1")  # default value
 
-        self.dropdown_menu = ttk.Combobox(self.control_frame, textvariable=self.dropdown_var, state="readonly")
+        self.dropdown_menu = customtkinter.CTkComboBox(self.control_frame, variable=self.dropdown_var, state="readonly")
         self.dropdown_menu.pack(side=tk.RIGHT, padx=10, pady=10)
         
-        # Load thumbnails and bind listbox selection
-        #self.load_thumbnails()
+        # Load thumbnails and bind listbox selection        
         self.load_presets()
 
+        self.draw_ui_preset_list()
+
+        self.selected_preset = self.preset_list[0]
         
         # No address provided, scan for devices
-        self.refresh()
+        #asyncio.create_task(self.refresh())
 
         #test = preset1.Presetx(status="Available")
         #test = preset1.Presetx(name="Tyler Bules", title="Electrical Engineering Manager", status="Out")
         #test.im.show()
         #self.listbox.bind('<<ListboxSelect>>', self.on_thumbnail_select)
 
-    def refresh(self):
-        print("Refreshing...")
-        asyncio.run(self.scan_for_devices())
-        #asyncio.create_task(self.scan_for_devices())
+    def upload(self):
+        print("Uploading...")
 
     def _on_mouse_wheel(self, event):
         self.left_canvas.yview_scroll(int(-1*(event.delta/120)), "units")
@@ -151,48 +126,80 @@ class App:
                 except Exception as e:
                     print(f"Error loading preset: {e}")
 
+            self.selected_preset = self.preset_list[0]
+
+    def draw_ui_preset_list(self):
+        #for widget in self.preset_list_frame.winfo_children():
+        #    widget.destroy()
+
+        left_frame_width = self.preset_list_frame.cget("width")
+
+        padding = 20
+
         for preset in self.preset_list:
             #render image
             preset.render()
             
             #img = Image.open(thumbnail_file)
             thumbnail_img = preset.im.copy()
-            thumbnail_img.thumbnail((180, 200))
+            thumbnail_img.thumbnail((left_frame_width-(2*padding), left_frame_width-(2*padding)))
             thumbnail_img = ImageTk.PhotoImage(thumbnail_img)
             #self.image_list.append(img)
             #self.thumbnail_list.append(thumbnail_img)
-            label = tk.Label(self.scrollable_frame, image=thumbnail_img, width=200, height=120, bg=bg_color)
+            label = customtkinter.CTkLabel(self.preset_list_frame, image=thumbnail_img, compound="top", width=left_frame_width, height=120, padx=10, pady=0)
+            if preset == self.selected_preset:
+                label.configure(fg_color="red")
             label.image = thumbnail_img
-            label.pack(padx=0, pady=0)
-            label.bind("<Button-1>", lambda e, preset=preset: [self.thumbnail_select(preset, self.change_bg_color(label))])
+            label.preset = preset
+            label.pack(side= tk.TOP, fill=tk.X, expand=True)
+            #label.bind("<Button-1>", lambda e, preset=preset: [self.thumbnail_select(preset, self.change_bg_color(label))])
+            label.bind("<Button-1>", command=self.thumbnail_select)
         
         # Update the scroll region after loading all thumbnails
-        self.left_canvas.configure(scrollregion=self.scrollable_frame.bbox("all"))
+        #self.preset_list_frame.configure(scrollregion=self.scrollable_frame.bbox("all"))
 
-    def thumbnail_select(self, preset, label):
+    def thumbnail_select(self, event):        
+        #find the index of previously selected preset
+        selected_index = self.preset_list.index(self.selected_preset)
+
+        #set fg color of previously selected widget to transparent
+        self.preset_list_frame.winfo_children()[selected_index].configure(fg_color='transparent')
+
+        #set fg color of selected widget to highlight it
+        event.widget.master.configure(fg_color="red")
+
+        #set new selected preset
+        self.selected_preset = event.widget.master.preset
+
         #render full size image into preview tab
-        preset.render()
-        img=ImageTk.PhotoImage(preset.im)
+        self.selected_preset.render()
+        img=ImageTk.PhotoImage(self.selected_preset.im)
         self.canvas.create_image(0, 0, anchor=tk.NW, image=img)
         self.canvas.image = img
 
         #erase all widgets in customize tab
-        for widget in self.customize_tab.winfo_children():
-            widget.destroy()
+        #for widget in self.tabControl.tab("Customize").winfo_children():
+        #    widget.destroy()
 
         #render configurable fields into customize tab
         #preset_fields = img.preset.fields
-        preset_fields = ['name', 'title', 'badge', 'status', 'banner_text']
-        for field in preset_fields:
-            label = tk.Label(self.customize_tab, text=field, bg=bg_color, fg=fg_color)
-            label.pack(anchor='w', padx=10, pady=5)
-            entry = tk.Entry(self.customize_tab)
-            entry.pack(anchor='w', padx=10, pady=5)
+        #preset_fields = ['name', 'title', 'badge', 'status', 'banner_text']
+        #for field in preset_fields:
+        #    label = customtkinter.CTkLabel(self.tabControl.tab("Customize"), text=field)
+        #    label.pack(anchor='w', padx=10, pady=5)
+        #    entry = customtkinter.CTkEntry(self.tabControl.tab("Customize"))
+        #    entry.pack(anchor='w', padx=10, pady=5)
             #entry.insert(0, getattr(img.preset, field, ''))
+
+        
 
     def change_bg_color(self, label):
         label.config(bg="white")
         label.update()
+
+    async def refresh(self):
+        print("Refreshing...")
+        await self.scan_for_devices()
 
     # Define a function to scan for BLE devices
     async def scan_for_devices(self):
@@ -217,8 +224,10 @@ class App:
         except Exception as e:
             print(f"Error during scan: {e}")
             return []
+        
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = App(root)
-    root.mainloop()
+    #root = tk.Tk()
+    app = App()
+    #asyncio.run(app.refresh())
+    app.mainloop()
