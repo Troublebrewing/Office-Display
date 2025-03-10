a6273292aac0a63f7199f64ba111329a5961f187
Tyler Bules <tbules@dixonvalve.com>
2025-03-10 14:50:18 -0400

    uploading now works. could probably be cleaned up still. testing icon placement

--- server2.py
+++ server2.py
@@ -9,6 +9,10 @@ import asyncio
 import threading
 from bleak import BleakClient, BleakScanner
 
+#library to pack image data for waveshare display
+#this is from the rpi library from the display manufacturer
+import epd7in3f
+
 class PresetListFrame(customtkinter.CTkScrollableFrame):
     def __init__(self, master):
         super().__init__(master)        
@@ -18,7 +22,9 @@ class PresetListFrame(customtkinter.CTkScrollableFrame):
 
 class App(customtkinter.CTk):
     def __init__(self):
-        super().__init__()
+        super().__init__()        
+        
+        self.ble_services = BLE_SERVICES()
         
         #self.root = root
         self.geometry("1020x560")        
@@ -56,8 +62,21 @@ class App(customtkinter.CTk):
         self.control_frame = customtkinter.CTkFrame(self.right_frame, fg_color="transparent")
         self.control_frame.grid(row=1, column=0, padx=0, pady=0, sticky="ew")
 
-        self.refresh_button = customtkinter.CTkButton(self.control_frame, text="Refresh", command=self.refresh)
+        self.refresh_button_image = ImageTk.PhotoImage(Image.open("icons/refresh.png").resize((20, 20)))
+        self.refresh_button = customtkinter.CTkButton(self.control_frame, text=None, image=self.refresh_button_image, command=self.refresh)
         self.refresh_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+        self.duplicate_button_image = ImageTk.PhotoImage(Image.open("icons/copy.png").resize((20, 20)))
+        self.duplicate_button = customtkinter.CTkButton(self.control_frame, text=None, image=self.duplicate_button_image, command=self.refresh)
+        self.duplicate_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+        self.remove_button_image = ImageTk.PhotoImage(Image.open("icons/delete.png").resize((20, 20)))
+        self.remove_button = customtkinter.CTkButton(self.control_frame, text=None, image=self.remove_button_image, command=self.refresh)
+        self.remove_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+        self.add_button_image = ImageTk.PhotoImage(Image.open("icons/add.png").resize((20, 20)))
+        self.add_button = customtkinter.CTkButton(self.control_frame, text=None, image=self.add_button_image, command=self.refresh)
+        self.add_button.pack(side=tk.LEFT, padx=10, pady=10)
 
         self.upload_button = customtkinter.CTkButton(self.control_frame, text="Upload", command=self.upload)
         self.upload_button.pack(side=tk.RIGHT, padx=10, pady=10)
@@ -94,13 +113,14 @@ class App(customtkinter.CTk):
         self.selected_bt_device
 
         #encode into datastream for waveshare display
-        wavesharebuf = epd.getbuffer(im)
+        epd = epd7in3f.EPD()
+        wavesharebuf = epd.getbuffer(self.selected_preset.im)
 
         #image_bytes = bytes(wavesharebuf)
         image_bytearray = bytearray(wavesharebuf)
         #print(f"image bytes: {image_bytearray}\n")
         
-        RLE_image_bytearray = run_length_encode2(image_bytearray)
+        RLE_image_bytearray = self.run_length_encode2(image_bytearray)
         #RLE_image_bytearray = run_length_encode2(image_bytearray)
         #print(f"encoded image: {RLE_image_bytearray}\n")
         #bytearray_compare(RLE_image_bytearray, RLE_image_bytearray2)
@@ -136,82 +156,10 @@ class App(customtkinter.CTk):
         #decode_test = run_length_decode(encode_test)
         #print(f"decoded test length:{len(decode_test)}\n")
         #print(f"decoded test: {decode_test}\n")
-        await send_bytes_to_client(RLE_image_bytearray)
 
-    # Define an async function to send a message to the device
-    async def send_bytes_to_client(databytes):
-        global response_received
-        global bytes_received
-
-        max_retries = 5
-        timeout_ms = 1000
-        bytes_received = 0
-
-        try:
-            image_size = len(run_length_decode(databytes))
-            # Check if the BleakClient object is available globally
-            if "client" in globals():
-                await client.connect()
-
-                
-                # Check if the client is connected
-                if client.is_connected:
-                    await client.start_notify(TX_FIFO_UUID, notification_handler)
-
-                    print("Sending data to BLE Client")
-                    image_bytes_sent = 0
-                    #or i in range(0, len(databytes), 20):
-                    while bytes_received < len(databytes):
-                        #chunk = databytes[i:i+20]
-                        if bytes_received+20 < len(databytes):
-                            chunk = databytes[bytes_received:bytes_received+20]
-                        else:
-                            chunk = databytes[bytes_received:]
-
-                        decoded_chunk = run_length_decode(chunk)
-                        image_bytes_sent = image_bytes_sent + len(decoded_chunk)
-                        retries = 0 #initialize retries for each chunk
-
-                        while retries < max_retries:
-                            try:
-                                # Wait for the acknowledgment before proceeding
-                                response_received = False  # Reset flag before waiting
-                                                            
-                                # Send the message to the RXUUID of the Bluetooth device
-                                await client.write_gatt_char(RX_FIFO_UUID, chunk)
-                                
-                                print(f"Transmitted: {bytes_received+len(chunk)}/{len(databytes)} bytes. Expanded: {image_bytes_sent}/{image_size}")
-                                
-                                try:
-                                    await asyncio.wait_for(wait_for_response(), timeout_ms / 1000)
-                                    if response_received:
-                                        print(f"Client has received:{bytes_received} bytes")
-                                        break
-                                except asyncio.TimeoutError:
-                                    print(f"No response received within {timeout_ms}ms, retrying... (Attempt {retries+1}/{max_retries})")
-                                    retries += 1  # Increment retries if timeout occurs
-                                
-                            except Exception as e:
-                                print(f"Error sending chunk: {e}")
-                                retries += 1  # Increment retries on exception
-                        
-                        if retries == max_retries:
-                            print(f"Max retries reached for chunk. Aborting.")
-                            break
-
-                    print("Data transmission complete\n")
-
-                    # Stop notifications when done
-                    # await client.stop_notify(RX_FIFO_UUID)
-
-                    await client.disconnect()
-                    return  # Exit function after successful transmission
-
-                else:
-                    print("Device is not connected.")
-
-        except Exception as e:
-            print(f"Error sending message to device: {e}")
+        #asyncio.run_coroutine_threadsafe(self.ble_services.send_bytes_to_client(databytes=RLE_image_bytearray, client_name=self.selected_bt_device), self.loop)
+        asyncio.run_coroutine_threadsafe(self.ble_services.send_bytes_to_client(databytes=RLE_image_bytearray, client_name=self.dropdown_menu.get()), self.loop)
+        #await self.send_bytes_to_client(RLE_image_bytearray) 
 
     def _on_mouse_wheel(self, event):
         self.left_canvas.yview_scroll(int(-1*(event.delta/120)), "units")
@@ -320,9 +268,155 @@ class App(customtkinter.CTk):
 
     def refresh(self):
         print("Refreshing...")
-        asyncio.run_coroutine_threadsafe(self.scan_for_devices(), self.loop)
-        #asyncio.run(self.scan_for_devices())
+        
+        #scan for devices and populate dropdown menu
+        asyncio.run_coroutine_threadsafe(self.populate_device_list(), self.loop)
+    
+    async def populate_device_list(self):
+        await self.ble_services.scan_for_devices()
 
+        print("Available devices:")
+        for i, device in enumerate(self.ble_services.named_bt_devices_found):
+            print(f"{i + 1}. {device.name} - {device.address}")
+            self.dropdown_menu.configure(values = [device.name for device in self.ble_services.named_bt_devices_found])
+        
+    def run_event_loop(self):
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_forever()
+
+    def run_length_encode2(self, data):
+        """
+        Perform run-length encoding on a bytes or bytearray object.
+
+        Parameters:
+            data: A bytes or bytearray object to encode.
+
+        Returns:
+            A bytearray containing the run-length encoded data.
+        """
+        if not isinstance(data, (bytes, bytearray)):
+            raise TypeError("Input data must be of type bytes or bytearray")
+        
+        # Initialize an empty result list
+        encoded = bytearray()
+
+        # Handle the case of empty input
+        if len(data) == 0:
+            return encoded
+
+        # Start the run-length encoding process
+        encoded.append(data[0])
+        count = 1
+        i=1
+
+        for i in range(1, len(data)):
+            
+            if(encoded[-1] == data[i]):
+                #same byte, increment counter
+                count += 1
+                if(count == 256):
+                    #write count then the same character again
+                    encoded.append(count-1)
+                    count = 1
+                    encoded.append(encoded[-2])
+            else:
+                encoded.append(count)
+                count = 1
+                encoded.append(data[i])
+        
+        encoded.append(count)
+
+        return encoded
+
+    
+
+class BLE_SERVICES:
+    def __init__(self, client=None):
+        # UUIDs for the virtual serial port
+        self.SERVICE_UUID = "569a1101-b87f-490c-92cb-11ba5ea5167c"
+        self.TX_FIFO_UUID = "569a2000-b87f-490c-92cb-11ba5ea5167c"  # Transmit characteristic
+        self.RX_FIFO_UUID = "569a2001-b87f-490c-92cb-11ba5ea5167c"  # Receive characteristic
+
+        self.client = client
+
+    # Define an async function to send a message to the device
+    async def send_bytes_to_client(self, databytes, client_name=None):
+        global response_received
+        global bytes_received
+
+        if client_name != None:
+            self.set_client(client_name)
+
+        max_retries = 5
+        timeout_ms = 1000
+        bytes_received = 0
+
+        try:
+            image_size = len(self.run_length_decode(databytes))
+            # Check if the BleakClient object is available globally
+            if "client" != None:
+                await self.client.connect()
+
+                
+                # Check if the client is connected
+                if self.client.is_connected:
+                    await self.client.start_notify(self.TX_FIFO_UUID, self.notification_handler)
+
+                    print("Sending data to BLE Client")
+                    image_bytes_sent = 0
+                    #or i in range(0, len(databytes), 20):
+                    while bytes_received < len(databytes):
+                        #chunk = databytes[i:i+20]
+                        if bytes_received+20 < len(databytes):
+                            chunk = databytes[bytes_received:bytes_received+20]
+                        else:
+                            chunk = databytes[bytes_received:]
+
+                        decoded_chunk = self.run_length_decode(chunk)
+                        image_bytes_sent = image_bytes_sent + len(decoded_chunk)
+                        retries = 0 #initialize retries for each chunk
+
+                        while retries < max_retries:
+                            try:
+                                # Wait for the acknowledgment before proceeding
+                                response_received = False  # Reset flag before waiting
+                                                            
+                                # Send the message to the RXUUID of the Bluetooth device
+                                await self.client.write_gatt_char(self.RX_FIFO_UUID, chunk)
+                                
+                                print(f"Transmitted: {bytes_received+len(chunk)}/{len(databytes)} bytes. Expanded: {image_bytes_sent}/{image_size}")
+                                
+                                try:
+                                    await asyncio.wait_for(self.wait_for_response(), timeout_ms / 1000)
+                                    if response_received:
+                                        print(f"Client has received:{bytes_received} bytes")
+                                        break
+                                except asyncio.TimeoutError:
+                                    print(f"No response received within {timeout_ms}ms, retrying... (Attempt {retries+1}/{max_retries})")
+                                    retries += 1  # Increment retries if timeout occurs
+                                
+                            except Exception as e:
+                                print(f"Error sending chunk: {e}")
+                                retries += 1  # Increment retries on exception
+                        
+                        if retries == max_retries:
+                            print(f"Max retries reached for chunk. Aborting.")
+                            break
+
+                    print("Data transmission complete\n")
+
+                    # Stop notifications when done
+                    # await client.stop_notify(RX_FIFO_UUID)
+
+                    await self.client.disconnect()
+                    return  # Exit function after successful transmission
+
+                else:
+                    print("Device is not connected.")
+
+        except Exception as e:
+            print(f"Error sending message to device: {e}")
+    
     # Define a function to scan for BLE devices
     async def scan_for_devices(self):
         try:
@@ -336,29 +430,60 @@ class App(customtkinter.CTk):
                 print("No devices with names found.")
                 return None
             
-            print("Available devices:")
-            for i, device in enumerate(self.named_bt_devices_found):
-                print(f"{i + 1}. {device.name} - {device.address}")
-                self.dropdown_menu.configure(values = [device.name for device in self.named_bt_devices_found])
-
-
+            return self.named_bt_devices_found
 
         except Exception as e:
             print(f"Error during scan: {e}")
             return []
         
-    def run_event_loop(self):
-        asyncio.set_event_loop(self.loop)
-        self.loop.run_forever()
+    def set_client(self, client_name):
+        for device in self.named_bt_devices_found:
+            if device.name == client_name:
+                self.client = BleakClient(device)
 
-    #def start_async_task(self):
-        #asyncio.run_coroutine_threadsafe(self.handle_async_task(), self.loop)
-    
-    #async def handle_async_task(self):
-        #self.label.configure(text="Running...")
-        #result = await self.async_task()
-        #self.label.configure(text=result)
+            print(f'Client set to {device.name}')
+            break
+
+    def run_length_decode(self, data):
+        # Initialize an empty result list
+        decoded = bytearray()
+
+        # Iterate over the encoded data two bytes at a time (count, value)
+        i = 0
+        while i < len(data):
+            count = data[i + 1]     # First byte is the count
+            value = data[i] # Second byte is the value
+            decoded.extend([value] * count)  # Replicate 'value' by 'count' times
+            i += 2
+
+        return decoded    
+
+    # Callback to handle received notifications
+    def notification_handler(self, sender, data):
+        """
+        Callback function that is called when data is received from the Bluetooth device.
         
+        Parameters:
+            sender: The sender of the notification (characteristic UUID).
+            data: The data received in bytes.
+        """
+        
+        global response_received
+        global bytes_received
+        
+        if b"rx:" in data:
+            response_received = True
+            bytes_received = int(data[3:].decode('ascii'), 16)
+        else:
+            print(f"Unexpected response from {sender}: {data}")    
+
+    async def wait_for_response(self):
+        """
+        A helper function that waits for the response_received flag to be set to True.
+        """
+        global response_received
+        while not response_received:
+            await asyncio.sleep(0.01)  # Small sleep to avoid busy-waiting
 
 if __name__ == "__main__":
     #root = tk.Tk()
